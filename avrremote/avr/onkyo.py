63a746d2ef54eb4d608eeb36ff6b100f26053a11
Woetsel <woetsel@users.noreply.github.com>
2017-05-25 19:45:32 +0200

    multi zone support + use of receiver.command instead of receiver.raw

--- avrremote/avr/onkyo.py
+++ avrremote/avr/onkyo.py
@@ -7,27 +7,31 @@ class Onkyo(AbstractAvr):
 	def __init__(self, config, listener):
 		self.listener = listener
 		self.ip = config['ip']
+		self.zones = ['main', 'zone2']
 		
 	@property
 	def static_info(self):
-		return { 'name': 'Onkyo', 'ip': self.ip, 'zones': [ 'zone1', 'zone2' ], 'sources': [ 'CBL/SAT', 'DVD', 'Blu-Ray' ], 'volume_step': 1 }
+		return { 'name': 'Onkyo', 'ip': self.ip, 'zones': self.zones, 'sources': [ 'CBL/SAT', 'DVD', 'Blu-Ray' ], 'volume_step': 1 }
 	
 	def get_power(self, zoneId):
 		with eiscp.eISCP(self.ip) as receiver:
-			return receiver.command('power query')
+			resp = receiver.command('power', arguments=['query'], zone=self.zones[zoneId])
+			return resp[1] == 'on'
 	
 	def set_power(self, zoneId, value):
 		with eiscp.eISCP(self.ip) as receiver:
-			return receiver.command('power on' if value else 'power off' )
+			resp = receiver.command('power', ['on' if value else 'standby'], zone=self.zones[zoneId])
+			print(resp)
+			return resp
 
 	def get_volume(self, zoneId):
 		with eiscp.eISCP(self.ip) as receiver:
-			resp = receiver.raw('MVLQSTN')
-		return int(resp[3:], 16)	
+			resp = receiver.command('volume', arguments=['query'], zone=self.zones[zoneId])
+		return resp[1]	
 	
 	def set_volume(self, zoneId, value):
 		with eiscp.eISCP(self.ip) as receiver:
-			return receiver.raw('MVL' + '{0:02X}'.format(value))
+			return receiver.command('volume', [str(value)], zone=self.zones[zoneId])
 	
 	def get_selected_input(self, zoneId):
 		return 0;
