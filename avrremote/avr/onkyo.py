540096052b29e0fa941480b15fdba35b0007e05b
Woetsel <woetsel@users.noreply.github.com>
2017-05-26 23:20:49 +0200

    added icon support

--- avrremote/avr/onkyo.py
+++ avrremote/avr/onkyo.py
@@ -8,10 +8,12 @@ class Onkyo(AbstractAvr):
 		self.listener = listener
 		self.ip = config['ip']
 		self.zones = ['main', 'zone2']
+		self.sources = [ ('dvd', 'hdmi'), ('fm', 'radio'), ('network', 'cloud'), ('am', 'radio'), ('strm-box', 'hdmi'), ('video2', 'hdmi'), ('bluetooth', 'bluetooth_audio') ]
+		self.source_ids = [x[0] for x in self.sources]
 			
 	@property
 	def static_info(self):
-		return { 'name': 'Onkyo', 'ip': self.ip, 'zones': self.zones, 'sources': [ ('CBL/SAT', 'settings_input_hdmi'), ('DVD', 'album'), ('Blu-Ray', 'album') ], 'volume_step': 1 }
+		return { 'name': 'Onkyo', 'ip': self.ip, 'zones': self.zones, 'sources': self.sources , 'volume_step': 1 }
 	
 	def get_power(self, zoneId):
 		with eiscp.eISCP(self.ip) as receiver:
@@ -21,7 +23,6 @@ class Onkyo(AbstractAvr):
 	def set_power(self, zoneId, value):
 		with eiscp.eISCP(self.ip) as receiver:
 			resp = receiver.command('power', ['on' if value else 'standby'], zone=self.zones[zoneId])
-			print(resp)
 			return resp
 
 	def get_volume(self, zoneId):
@@ -34,7 +35,10 @@ class Onkyo(AbstractAvr):
 			return receiver.command('volume', [str(value)], zone=self.zones[zoneId])
 	
 	def get_selected_input(self, zoneId):
-		return 0;
+		with eiscp.eISCP(self.ip) as receiver:
+			resp = receiver.command('input-selector' if zoneId == 0 else 'selector', arguments=['query'], zone=self.zones[zoneId])
+			sourceid = resp[1][0] if isinstance(resp[1], tuple) else resp[1]
+		return self.source_ids.index(sourceid)
 	
 	def select_input(self, zoneId, inputId):
 		pass
