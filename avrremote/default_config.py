206cc00141ab14f8c5ee9c860b9c44e5133f1fbc
drhoet <drhoet@users.noreply.github.com>
2017-06-11 19:02:32 +0200

    Removing unused properties from default_config.py

--- avrremote/default_config.py
+++ avrremote/default_config.py
@@ -2,6 +2,4 @@ default_config = {
   'avr_module': 'avrremote.avr.base',
   'avr_class': 'AbstractAvr',
   'avr_connection': { 'ip': '192.168.0.55' },
-  'max_volume': 60.0,
-  'rotation': 'clockwise',
 }
