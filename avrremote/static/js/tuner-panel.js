e901f609ffafdb031f4205ad05a5ade5c39c58a0
drhoet <drhoet@users.noreply.github.com>
2017-06-25 22:01:13 +0200

    Tuner seeking -- UI part

--- avrremote/static/js/tuner-panel.js
+++ avrremote/static/js/tuner-panel.js
@@ -229,36 +229,6 @@ collector.register($.get('templates/tuner-panel.html', function(template) {
 			}
 		},
 		computed: {
-			freqMin: function() {
-				switch (this.item.band) {
-					case "AM":
-						return 522;
-					case "FM":
-						return 87.50;
-					default:
-						return 0;
-				}
-			},
-			freqMax: function() {
-				switch (this.item.band) {
-					case "AM":
-						return 1611;
-					case "FM":
-						return 108;
-					default:
-						return 0;
-				}
-			},
-			freqStep: function() {
-				switch (this.item.band) {
-					case "AM":
-						return 9;
-					case "FM":
-						return 0.05;
-					default:
-						return 0;
-				}
-			},
 			freqUnit: function() {
 				switch (this.item.band) {
 					case "AM":
